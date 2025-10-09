b1817d37e7ac76d2d56390a8f0eb16e1c897735d
luixfelipe0 <luiz_santos1811@outlook.com>
2025-10-09 17:08:14 -0300

    project created

---
+++ src/test/java/com/luix/spring_product_management_api/SpringProductManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luix.spring_product_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
