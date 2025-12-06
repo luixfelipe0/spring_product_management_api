b2a75a854cb03d6b24e6f2a77496df1808707eca
luix <luiz_santos1811@outlook.com>
2025-12-06 12:48:45 -0300

    feat: add User controller with all endpoints

---
+++ src/main/java/com/luix/ecommerce/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.luix.ecommerce.controller;
+
+import com.luix.ecommerce.dto.UserRequestDTO;
+import com.luix.ecommerce.dto.UserResponseDTO;
+import com.luix.ecommerce.dto.UserUpdateDTO;
+import com.luix.ecommerce.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createUser(dto));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserResponseDTO>> findAll() {
+        return ResponseEntity.ok(service.findAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> update(@PathVariable Long id, @RequestBody UserUpdateDTO dto) {
+        return ResponseEntity.ok(service.updateUser(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deactivate(@PathVariable Long id) {
+        service.deactivateUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
