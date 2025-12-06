cb429c7c16ef3ad6f4666569d1bd381c5b36009a
luix <luiz_santos1811@outlook.com>
2025-12-06 10:42:35 -0300

    feat: add User mapper with dto convertors

---
+++ src/main/java/com/luix/ecommerce/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package com.luix.ecommerce.mapper;
+
+import com.luix.ecommerce.dto.UserRequestDTO;
+import com.luix.ecommerce.dto.UserResponseDTO;
+import com.luix.ecommerce.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserRequestDTO dto) {
+        User user = new User();
+        user.setName(dto.name());
+        user.setEmail(dto.email());
+        user.setPassword(dto.password());
+        user.setPhone(dto.phone());
+        user.setActive(true);
+        return user;
+    }
+
+    public UserResponseDTO toDto(User user) {
+        return new UserResponseDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPhone()
+        );
+    }
+}
