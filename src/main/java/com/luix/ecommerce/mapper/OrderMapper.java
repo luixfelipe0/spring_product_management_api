b13d6d21d8b640b5de31d3be4b45609f20bc6681
luix <luiz_santos1811@outlook.com>
2025-12-09 17:59:46 -0300

    feat: add Order and OrderItem mapper

---
+++ src/main/java/com/luix/ecommerce/mapper/OrderMapper.java
@@ -0,0 +1,55 @@
+package com.luix.ecommerce.mapper;
+
+import com.luix.ecommerce.dto.order.OrderItemRequestDTO;
+import com.luix.ecommerce.dto.order.OrderItemResponseDto;
+import com.luix.ecommerce.dto.order.OrderRequestDTO;
+import com.luix.ecommerce.dto.order.OrderResponseDTO;
+import com.luix.ecommerce.entity.Order;
+import com.luix.ecommerce.entity.OrderItem;
+import com.luix.ecommerce.entity.Product;
+import com.luix.ecommerce.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderMapper {
+
+    public Order toEntity(OrderRequestDTO dto, User client) {
+        Order order = new Order();
+        order.setClient(client);
+        return order;
+    }
+
+    public OrderItem toEntityItem(OrderItemRequestDTO dto, Order order, Product product) {
+        return new OrderItem(
+                order,
+                product,
+                dto.quantity(),
+                dto.price()
+        );
+    }
+
+    public OrderResponseDTO toDto(Order order) {
+        return new OrderResponseDTO(
+                order.getId(),
+                order.getCreatedAt(),
+                order.getOrderStatus().getValue(),
+                order.getClient().getId(),
+                order.getItems()
+                        .stream()
+                        .map(this::toDtoItem)
+                        .toList(),
+                order.getTotal()
+        );
+    }
+
+    public OrderItemResponseDto toDtoItem(OrderItem item) {
+        return new OrderItemResponseDto(
+                item.getProduct().getId(),
+                item.getProduct().getName(),
+                item.getQuantity(),
+                item.getPrice(),
+                item.getSubTotal()
+        );
+    }
+
+}
