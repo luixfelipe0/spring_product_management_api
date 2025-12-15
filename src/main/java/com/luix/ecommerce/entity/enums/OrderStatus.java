d8aff1f8c6c91d7942c752a8fd94ae9815272e45
luix <luiz_santos1811@outlook.com>
2025-12-15 06:20:55 -0300

    refactor(order-status): add field code for OrderStatus

--- src/main/java/com/luix/ecommerce/entity/enums/OrderStatus.java
+++ src/main/java/com/luix/ecommerce/entity/enums/OrderStatus.java
@@ -1,20 +1,37 @@
 package com.luix.ecommerce.entity.enums;
 
+import java.util.Objects;
+
 public enum OrderStatus {
 
-    WAITING_PAYMENT("WAITING_PAYMENT"),
-    PAID("PAID"),
-    SHIPPED("SHIPPED"),
-    DELIVERED("DELIVERED"),
-    CANCELED("CANCELED");
+    WAITING_PAYMENT(1, "WAITING_PAYMENT"),
+    PAID(2, "PAID"),
+    SHIPPED(3, "SHIPPED"),
+    DELIVERED(4,"DELIVERED"),
+    CANCELED(5, "CANCELED");
 
+    private final Integer code;
     private final String value;
 
-    OrderStatus(String value) {
+    OrderStatus(Integer code, String value) {
+        this.code = code;
         this.value = value;
+    }
+
+    public Integer getCode() {
+        return code;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public static OrderStatus valueOf(Integer code) {
+        for (OrderStatus status : OrderStatus.values()) {
+            if (Objects.equals(status.getCode(), code)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Order Status code: " + code);
     }
 }
