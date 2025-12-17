e471d440dd1aee2a6ca9564381bca016b3fd282b
luix <luiz_santos1811@outlook.com>
2025-12-17 06:46:39 -0300

    feat(security): add authenticationManager into SecurityConfig

--- src/main/java/com/luix/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/luix/ecommerce/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -65,6 +67,11 @@ public class SecurityConfig {
                 );
 
         return https.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
