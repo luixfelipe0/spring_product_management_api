c08d9d39487350159862fe77bdea4a762f52fde5
luix <luiz_santos1811@outlook.com>
2025-12-19 09:18:29 -0300

    feat(exception): expand GlobalExceptionHandler

--- src/main/java/com/luix/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/luix/ecommerce/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.luix.ecommerce.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
 import java.time.Instant;
 
 @RestControllerAdvice
@@ -34,6 +37,33 @@ public class GlobalExceptionHandler {
     public ResponseEntity<StandardExceptionMessage> validationHandler(MethodArgumentNotValidException e, HttpServletRequest request) {
         String error = "Validation error.";
         HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardExceptionMessage err = new StandardExceptionMessage(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<StandardExceptionMessage> accessDenied(AccessDeniedException e, HttpServletRequest request) {
+        String error = "You don't have the permission to execute this action.";
+        HttpStatus status = HttpStatus.FORBIDDEN;
+
+        StandardExceptionMessage err = new StandardExceptionMessage(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<StandardExceptionMessage> badCredentials(BadCredentialsException e, HttpServletRequest request) {
+        String error = "Incorrect e-mail or password.";
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        StandardExceptionMessage err = new StandardExceptionMessage(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardExceptionMessage> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found.";
+        HttpStatus status = HttpStatus.NOT_FOUND;
 
         StandardExceptionMessage err = new StandardExceptionMessage(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
